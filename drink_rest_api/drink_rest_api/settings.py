9f804617ca46a47e4771a1271b431e9102b1ab90
Alexis Peves <alexpeves@gmail.com>
2022-09-13 16:52:41 -0400

    login backend done

--- drink_rest_api/drink_rest_api/settings.py
+++ drink_rest_api/drink_rest_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,8 +100,8 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 ############
 # Bcrypt password Hashers
